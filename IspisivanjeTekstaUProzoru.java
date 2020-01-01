5c46818b2bf9136b408554969e6ef0d9430855d2
Byte-Block <55253959+Byte-Block@users.noreply.github.com>
2020-01-01 23:41:53 +0100

    Add files via upload

---
+++ IspisivanjeTekstaUProzoru.java
@@ -0,0 +1,30 @@
+package zadatak12_1;
+
+import java.awt.*;
+import java.awt.event.*;
+
+public class IspisivanjeTekstaUProzoru extends Frame {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	private IspisivanjeTekstaUProzoru() {
+		super("Ispisivanje Teksta U Prozoru");
+		setSize(300, 200);
+		add(new Label("Pozdrav svima!", Label.CENTER));
+		addWindowListener(new ProzorDogadjaji());
+		setVisible(true);
+	}
+
+	private class ProzorDogadjaji extends WindowAdapter {
+		@Override
+		public void windowClosing(WindowEvent dog) {
+			dispose();
+		}
+	}
+
+	public static void main(String[] varg) {
+		new IspisivanjeTekstaUProzoru();
+	}
+}
